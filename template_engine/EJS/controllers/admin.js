200eb2f8480806ee03554373af3b25ef9e306f51
RatkoG <ratko.gjurichanin@gmail.com>
2023-08-16 01:35:56 -0600

    editing product

--- template_engine/EJS/controllers/admin.js
+++ template_engine/EJS/controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
+      editing: false
     });
   };
   
@@ -11,9 +12,9 @@ exports.getAddProduct = (req, res, next) => {
     const title = req.body.title
     const imageUrl = req.body.imageUrl
     const price = req.body.price
-    const description = req.body.description
+    const description = req.body.description 
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect("/");
   };
@@ -22,13 +23,34 @@ exports.getAddProduct = (req, res, next) => {
     if (!editMode){
       return res.redirect('/')
     }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      // instead of passing boolean
-      editing: editMode
-    });
+
+    const  prodId = req.params.productId
+    Product.findById(prodId, product => {
+      if (!product){
+        return res.redirect('/')
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        // instead of passing boolean
+        editing: editMode,
+        product: product
+      });
+    })
   };
+
+exports.postEditProduct = (req, res, next) => {
+const prodId = req.body.productId
+const updatedTitle = req.body.title
+const updatedPrice = req.body.price
+const updatedImageUrl = req.body.imageUrl
+const updatedDescription = req.body.description
+const updatedProduct = new Product(prodId, updatedTitle, updatedPrice, updatedImageUrl, updatedDescription)
+
+updatedProduct.save()
+res.redirect('/admin/products')
+}
+
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
