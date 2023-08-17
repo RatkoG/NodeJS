e0c07be6488a33bef50d827867573005a326bbc0
RatkoG <ratko.gjurichanin@gmail.com>
2023-08-17 16:13:50 -0600

    creating association/relation

--- template_engine/EJS/app.js
+++ template_engine/EJS/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const app = express();
 
@@ -22,8 +24,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Creating Relation/Associations
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+// Define the inverse relationship
+User.hasMany(Product)
+
 // Sync syncs your modal to the database by creating the appropriate tables
-sequelize.sync().then(result => {
+// Don't use this force on production
+// This is for overwriting the tables
+sequelize.sync({force: true}).then(result => {
     // console.log(result); 
     app.listen(3000);
 }).catch(err => {
