93079e4ba29ce6c0cd880a17265b988f1a962e08
RatkoG <ratko.gjurichanin@gmail.com>
2023-08-19 09:04:37 -0600

    delete items

--- template_engine/EJS-mongoDB/routes/shop.js
+++ template_engine/EJS-mongoDB/routes/shop.js
@@ -19,6 +19,6 @@ router.post('/cart', shopController.postCart);
 
 // router.get('/orders', shopController.getOrders);
 
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct)
+router.post('/cart-delete-item', shopController.postCartDeleteProduct)
 
 module.exports = router;
