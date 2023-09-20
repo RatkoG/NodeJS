1b5807502a3da052897ce38154df747872512965
RatkoG <ratko.gjurichanin@gmail.com>
2023-09-19 22:13:51 -0600

    encrypted password

---  cookies-sessions/controllers/auth.js
+++  cookies-sessions/controllers/auth.js
@@ -1,66 +1,64 @@
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(err => {
+      req.session.save((err) => {
         console.log(err);
-        res.redirect('/');
+        res.redirect("/");
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {
-  const email = req.body.email
-  const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
-  // Check if user with email exist?
-  // With MongoDB we can add index on email field and give it unique property. This is one of the options to check if the email already exists in the database.
-  // Alternative is to find the user
-  User.findOne({email: email})
-    .then(userDoc => {
-      if(userDoc){
-        return res.redirect('/signup')
-      }
-      const user = new User({
-        email: email,
-        password: password,
-        cart: {items: []}
-      })
-      return user.save()
-    })
-    .then(result => {
-      res.redirect('/login')
-    })
-    .catch(err => {
-      console.log(err)
-    })
+exports.postSignup = async (req, res, next) => {
+  try {
+    const { email, password, confirmPassword } = req.body;
+    // Check if user with email exist?
+    // With MongoDB we can add index on email field and give it unique property. This is one of the options to check if the email already exists in the database.
+    // Alternative is to find the user
+    const userDoc = await User.findOne({ email });
 
-  
+    if (userDoc) {
+      return res.redirect("/signup");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] },
+    });
+
+    await user.save();
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
