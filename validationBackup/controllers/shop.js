593022b654c8e05719d8411b135fd26089921f2d
RatkoG <ratko.gjurichanin@gmail.com>
2023-09-21 19:41:44 -0600

    preparing pagination data on the server

--- validationBackup/controllers/shop.js
+++ validationBackup/controllers/shop.js
@@ -36,16 +36,30 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
-
+  let totalItems;
+  // This is mongoDB method to count the number of elements
   Product.find()
-    // This is mongoDB method to skip the first n elements and limit the number of elements returned
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .count()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return (
+        Product.find()
+          // This is mongoDB method to skip the first n elements and limit the number of elements returned
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE)
+      );
+    })
     .then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
